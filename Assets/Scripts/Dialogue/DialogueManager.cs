8e371d58f0513f52f8ab402f5ff4121a1cd30c0d
EvandroBustamante <evandro.bustamante@gmail.com>
2025-07-26 12:02:46 -0300

    Input name in dialogue

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -7,36 +7,59 @@ public class DialogueManager : MonoBehaviour
     public GameObject dialoguePanel;
     public TextMeshProUGUI dialogueText;
     public Button nextDialogueButton;
+    public TMP_InputField inputField;
+    public Button inputFieldButton;
     public Dialogue dialogue;
+    public Dialogue dialogue2;
+    public bool isLevel1 = false;
 
     private int dialogueLinesIndex;
+    private string playerName;
+    private Dialogue dialoguePlaying;
 
     private void Awake()
     {
         nextDialogueButton.onClick.AddListener(NextDialogue);
+        inputFieldButton.onClick.AddListener(StartDialogue2);
 
         dialoguePanel.SetActive(false);
+        inputField.gameObject.SetActive(false);
 
-        StartDialogue();
+        StartDialogue(dialogue);
     }
 
-    private void StartDialogue()
+    private void StartDialogue(Dialogue thisDialogue)
     {
+        dialoguePlaying = thisDialogue;
         dialoguePanel.SetActive(true);
         dialogueLinesIndex = 0;
-        dialogueText.text = dialogue.dialogueLines[dialogueLinesIndex];
+        string finalDialogue = dialoguePlaying.dialogueLines[dialogueLinesIndex].Replace("[PLAYERNAME]", playerName);
+        dialogueText.text = finalDialogue;
     }
 
     private void NextDialogue()
     {
-        if (dialogueLinesIndex < dialogue.dialogueLines.Length - 1)
+        if (dialogueLinesIndex < dialoguePlaying.dialogueLines.Length - 1)
         {
             dialogueLinesIndex++;
-            dialogueText.text = dialogue.dialogueLines[dialogueLinesIndex];
+            string finalDialogue = dialoguePlaying.dialogueLines[dialogueLinesIndex].Replace("[PLAYERNAME]", playerName);
+            dialogueText.text = finalDialogue;
         }
         else
         {
             dialoguePanel.SetActive(false);
+
+            if (isLevel1)
+            {
+                isLevel1 = false;
+                inputField.gameObject.SetActive(true);
+            }
         }
+    }
+     private void StartDialogue2()
+    {
+        playerName = inputField.text;
+        inputField.gameObject.SetActive(false);
+        StartDialogue(dialogue2);
     }
 }
